018d4ef3d7e618aee2aec401d138b3cea32c83c9
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-07-01 11:48:44 -0400

    version estable

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,15 +2,12 @@ const request = require("supertest");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
-    it("Obteniendo status code 200", async () => {
+    it("Obteniendo arreglo con por lo menos 1 objeto", async () => {
         const response = await request(server).get("/cafes").send();
         const status = response.statusCode;
-        expect(status).toBe(200);
-    })
+        const cafes = response.body;
 
-    it("Obteniendo arreglo con por lo menos 1 objeto", async () => {
-        const { body } = await request(server).get("/cafes").send();
-        const cafes = body;
+        expect(status).toBe(200);
         expect(cafes).toBeInstanceOf(Array);
         expect(cafes.length).toBeGreaterThan(0);
     })
@@ -24,5 +21,26 @@ describe("Operaciones CRUD de cafes", () => {
             .send();
         
             expect(response.statusCode).toBe(404);
+    })
+
+    it("Agregando nuevo café - status code 201", async () => {
+        const id = Math.floor(Math.random() * 999);
+        const cafe = {id, nombre: "Café Helado"};
+        const response = await request(server)
+            .post("/cafes")
+            .send(cafe);
+
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toContainEqual(cafe);
+    })
+
+    it("Actualizar café id inexistente", async () => {
+        const cafe = {id: "id", nombre: "Café Inexistente"};
+        const id = "id_inexistente";
+        const response = await request(server).
+            put(`/cafes/${id}`)
+            .send(cafe);
+        const status = response.statusCode;
+        expect(status).toBe(400);
     })
 });
